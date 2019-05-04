747ce19a8232c9c59dc7ae9e3294645d68fb1e78
CodrutaH <50257985+CodrutaH@users.noreply.github.com>
2019-05-04 12:56:03 +0300

    Add project files.

---
+++ WebApplication3/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication3_api.Models
+{
+    public class Product
+    {
+        //[Key()]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Colors { get; set; }
+        [Range(1, 10)]
+        public int SmellLevel { get; set; }
+        public bool IsArtificial { get; set; }
+    }
+}
---
+++ WebApplication3/Models/ProductsDbSeeder.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication3_api.Models;
+
+namespace WebApplication3.Models
+{
+    public class ProductsDbSeeder
+    {
+        public static void Initialize(ProductsDbContext context)
+        {
+            context.Database.EnsureCreated();
+
+            // Look for any flowers.
+            if (context.Products.Any())
+            {
+                return;   // DB has been seeded
+            }
+
+            context.Products.AddRange(
+                new Product
+                {
+                    Name = "Rose",
+                    Colors = "White, Yellow",
+                    IsArtificial = false,
+                    SmellLevel = 5
+                },
+                new Product
+                {
+                    Name = "Daisy",
+                    Colors = "Yellow, Red",
+                    IsArtificial = true,
+                    SmellLevel = 2
+                }
+            );
+            context.SaveChanges();
+        }
+    }
+}
+
+    
